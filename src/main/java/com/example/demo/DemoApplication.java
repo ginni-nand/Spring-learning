7693d2bdaec2d28b5d2e4c4c040e85f1b431b758
ginni-nand <ginni.nand@medly.com>
2022-10-28 23:04:34 +0530

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(DemoApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping
+    public String hello(){
+        return "Hello world";
+    }
+
+}
