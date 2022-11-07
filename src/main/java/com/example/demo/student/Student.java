318f09c13726911e6309e86011d96f09b1137abe
ginni-nand <ginni.nand@medly.com>
2022-11-07 11:29:55 +0530

    add annotation for student class

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,55 +1,108 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        }
+)
+@Entity(name = "student")
 public class Student {
+
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
-    private String name;
-    private Integer age;
-    private LocalDate dob;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+    @Column(
+            name = "email ",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String email;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private Integer age;
+    @Column(
+            name = "dob",
+            nullable = false
+    )
+    private LocalDate dob;
+
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
+    public Student(String firstName, String lastName, Integer age, String email, LocalDate dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.dob = dob;
     }
 
     @Override
     public String toString() {
         return "Student{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", dob=" + dob +
-                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age + '\'' +
+                ", email=" + email + '\''+
+                ", dob="+ dob +
                 '}';
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 
     public void setAge(Integer age) {
         this.age = age;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
     }
 
     public String getEmail() {
@@ -60,21 +113,11 @@ public class Student {
         this.email = email;
     }
 
-    public Student(Long id, String name, Integer age, LocalDate dob, String email) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.dob = dob;
-        this.email = email;
+    public LocalDate getDob() {
+        return dob;
     }
 
-    public Student(String name, Integer age, LocalDate dob, String email) {
-        this.name = name;
-        this.age = age;
+    public void setDob(LocalDate dob) {
         this.dob = dob;
-        this.email = email;
-    }
-
-    public Student() {
     }
 }
