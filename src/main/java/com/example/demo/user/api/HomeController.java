5185a7b36a5955c4af3c9aaa7ce009ec9838b30a
ginni-nand <ginni.nand@medly.com>
2022-11-19 14:03:54 +0530

    GN| add home controller

---
+++ src/main/java/com/example/demo/user/api/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.user.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String getHome() {
+        return "this is home page";
+    }
+}
--- src/main/java/com/example/demo/user/security/SecurityConfig.java
+++ src/main/java/com/example/demo/user/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.user.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/public/**").permitAll()
+                .anyRequest().authenticated().and().httpBasic();
     }
 }
